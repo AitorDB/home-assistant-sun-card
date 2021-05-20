ebf0f6dfd90824ea1081ec868b947666cbcde1c5
LiJu09 <lisojuraj@gmail.com>
2021-05-20 17:38:30 +0200

    Add Slovak translation

--- src/constants.ts
+++ src/constants.ts
@@ -11,6 +11,7 @@ import nl from './assets/localization/languages/nl.json'
 import pl from './assets/localization/languages/pl.json'
 import ptBR from './assets/localization/languages/pt-BR.json'
 import ru from './assets/localization/languages/ru.json'
+import sk from './assets/localization/languages/sk.json'
 import sl from './assets/localization/languages/sl.json'
 import sv from './assets/localization/languages/sv.json'
 import { TSunCardConfig, TSunCardTexts } from './types'
@@ -33,7 +34,7 @@ export class Constants {
 
   static readonly HORIZON_Y = 108
   static readonly LOCALIZATION_LANGUAGES: Record<string, TSunCardTexts> = {
-    da, de, en, es, et, fi, fr, hu, it, nl, pl, 'pt-BR': ptBR, ru, sl, sv
+    da, de, en, es, et, fi, fr, hu, it, nl, pl, 'pt-BR': ptBR, ru, sk, sl, sv
   }
   static readonly SUN_RADIUS = 17
 }
